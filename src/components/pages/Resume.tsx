d64d3f76462cf7619bfb5ec984c04a44cf587916
Joshua Hendrix <thejoshuahendrix@gmail.com>
2023-07-02 09:20:19 -0400

    changed object tag to iframe

--- src/components/pages/Resume.tsx
+++ src/components/pages/Resume.tsx
@@ -3,12 +3,11 @@ import ResumePDF from "../../assets/Resume2023.pdf";
 export const Resume = () => {
   return (
     <div style={{ height: "1600px", width: "100%" }}>
-      <object
-        data={ResumePDF}
-        type="application/pdf"
+      <iframe
+        src={ResumePDF}
         width="100%"
         height="100%"
-      ></object>
+      ></iframe>
     </div>
   );
 };
