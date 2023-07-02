9c691f57818b13270dd623737a7c3369c038b06b
Joshua Hendrix <thejoshuahendrix@gmail.com>
2023-07-02 09:28:35 -0400

    object tag with fallback

--- src/components/pages/Resume.tsx
+++ src/components/pages/Resume.tsx
@@ -3,11 +3,13 @@ import ResumePDF from "../../assets/Resume2023.pdf";
 export const Resume = () => {
   return (
     <div style={{ height: "1600px", width: "100%" }}>
-      <iframe
-        src={ResumePDF}
+      <object
+        data={ResumePDF}
+        type="application/pdf"
         width="100%"
         height="100%"
-      ></iframe>
+      ><p>Oops your browser doesn't support viewing this PDF</p>
+      <p><a href={ResumePDF}>Download Here Instead</a></p></object>
     </div>
   );
 };
